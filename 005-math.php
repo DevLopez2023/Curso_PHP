d932d85b101dea6d5a147a0fcda6114795cdbd98
devlopez <devlopez@outlook.es>
2025-09-05 09:15:30 -0500

    PHP

---
+++ 005-math.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+El módulo Math en PHP
+*/
+//pi()
+echo number_format(pi(),2); //3.14
+
+//max() & min()
+echo '<br>'.max(1,20,40,50); //50
+echo '<br>'.min(1,20,40,50); //1
+
+//abs()
+echo '<br>'.abs(-20); //20
+
+//sqrt()
+echo '<br>'.sqrt(81); //9
+
+//round()
+echo '<br>'.round(1.50); //2
+
+//floor()
+echo '<br>'.floor(1.50); //1
+
+//rand()
+echo '<br>'.rand(1,100); //?
+
+//pow()
+echo '<br>'.pow(2,3); //8
+
+//log()
+echo '<br>'.log(20); //2.9957..
+
+//log10()
+echo '<br>'.log10(20); //1.3010..
+
+//sin()
+echo '<br>'.sin(20); //0.9129..
+
+//cos()
+echo '<br>'.cos(20); //0.4080..
+
+//tan()
+echo '<br>'.tan(20); //2.2371..
